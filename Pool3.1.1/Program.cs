cde8f29df3099f6acad1ba50163c09b772505d3c
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2020-12-30 19:47:58 +0200

    Add files via upload

--- Pool3.1.1/Program.cs
+++ Pool3.1.1/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Pool2
+namespace Pool3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int a=0,b=0;
+            int[] n = new int[10];
+            for (int i = 0; i < 10; i++) {
+                n[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+
+
+            Console.WriteLine("1");
+            for(int j=0;j<10;j++)
+            { int w = 0;
+                w = n[j];
+                b = b + w;
+            }
+            Console.WriteLine(b);
+
         }
     }
 }
