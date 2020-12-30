2808506b702fb7897abce4294736b3547951dab9
ArdaiArtur <72787544+ArdaiArtur@users.noreply.github.com>
2020-12-30 20:10:42 +0200

    Add files via upload

---
+++ Pool3.1/Pool3.2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pool3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cate numere vrei sa aiba vectorul");
+            int a= Convert.ToInt32(Console.ReadLine()),k =5;
+            int[] n = new int[10];
+            for (int i = 1; i < a+1; i++) {
+                n[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+
+
+
+
+           
+            for(int j=1;j<a+1;j++)
+            {
+                int d = 0;
+                if (k == n[j] && d == 0) { Console.WriteLine("pozitia lui k", j); d++; }
+                else Console.WriteLine(-1);
+            }
+            
+
+        }
+    }
+}
